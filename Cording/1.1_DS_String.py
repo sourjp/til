880854043b4428544595603ed5a400abb491cf2a
sourjp <hiro0209.ki@gmail.com>
2019-12-14 12:20:24 +0900

    change file names

--- Cording/1.1_DS_String.py
+++ Cording/1.1_DS_String.py
@@ -31,6 +31,10 @@ test = [
 
 
 def check_dp_ascii(words):
+    ''' O(n)
+    最大でも O(128) までの範囲なので、O(1)とも言える。
+    '''
+
     dp_ascii = ['' for i in range(129)]
 
     for word in words:
@@ -45,6 +49,10 @@ def check_dp_ascii(words):
 
 
 def check_dp_unicode(words):
+    ''' O(n)
+    最大でも O(8**8) までの範囲。ただここまで入力が達するのは難しいので O(n)。
+    '''
+
     dp_ascii = ['' for i in range(8**8)]
 
     for word in words:
@@ -58,10 +66,30 @@ def check_dp_unicode(words):
     return 'GOOD: {} have no one more word'.format(words)
 
 
+def check_sort(words):
+    ''' O(n+logn)
+    sortでO(logn)
+    dataの数だけ実施するのでO(n)
+    '''
+
+    words_list = []
+    for w in words:
+        words_list.append(w)
+
+    words_list.sort()
+    for i in range(len(words)-1):
+        if words_list[i] == words_list[i+1]:
+            return 'BAD: {} have one more same word'.format(words)
+        else:
+            pass
+    
+    return 'GOOD: {} have no one more word'.format(words)
+
 
 if __name__ == "__main__":
     for w in test:
         print(check_dp_ascii(w))
         print(check_dp_unicode(w))
+        print(check_sort(w))
         print('---')
 
