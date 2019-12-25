69e6ab30d1f324971d09a5fc8a7397c6878d48bc
sourjp <hiro0209.ki@gmail.com>
2019-12-25 23:15:53 +0900

    start leetcode

---
+++ LeetCode/TwoSome.py
@@ -0,0 +1,19 @@
+'''
+Runtime: 280 ms, faster than 32.79% of Python3 online submissions for Two Sum.
+Memory Usage: 13.7 MB, less than 86.51% of Python3 online submissions for Two Sum.
+'''
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        memo = []
+        
+        for i in range(len(nums)):
+            want = target - nums[i]
+            if want in memo:
+                ans = [nums.index(want), i]
+                return ans
+            
+            memo.append(nums[i])
+        
+        return False
+    
