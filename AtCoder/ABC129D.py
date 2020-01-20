fb8d35905346058a7f3db735e3daea38310545b5
sourjp <hiro0209.ki@gmail.com>
2020-01-20 23:24:01 +0900

    atcoder

--- AtCoder/ABC129D.py
+++ AtCoder/ABC129D.py
@@ -1,5 +1,25 @@
+import numpy as np
+
 h, w = map(int, input().split())
-s = [list(input()) for _ in range(h)]
+s = np.array([list(input()) for _ in range(h)]) == '.'
+
+L = np.zeros((h, w), dtype=int)
+R = np.zeros((h, w), dtype=int)
+U = np.zeros((h, w), dtype=int)
+D = np.zeros((h, w), dtype=int)
+
+for i in range(h):
+    D[i] = (D[i-1]+1) * s[i]
+    U[-i-1] = (U[-i]+1) * s[-i-1]
+
+for i in range(w):
+    R[:, i] = (R[:, i-1]+1) * s[:, i]
+    L[:, -i-1] = (L[:, -i]+1) * s[:, -i-1]
+
+
+print(np.max(D+U+R+L)-3)
+
+'''
 
 L = [[0 for _ in range(w)] for _ in range(h)]
 R = [[0 for _ in range(w)] for _ in range(h)]
@@ -54,61 +74,11 @@ def make(L, R, U, D):
             j += 1
         i += 1
 
-'''
-    for j in range(w):
-        for i in range(h):
-            if s[i][j] == '#':
-                D[i][j] == 0
-            elif s[i][j] == '.':
-                if i == 0:
-                    D[i][j] = 1
-                else:
-                    D[i][j] = D[i-1][j] + 1
-
-            ui = h-i-1
-            uj = j
-            if s[ui][uj] == '#':
-                U[ui][uj] == 0
-            elif s[ui][uj] == '.':
-                if ui == h-1:
-                    U[ui][uj] = 1
-                else:
-                    U[ui][uj] = U[ui+1][uj] + 1
-'''
-
-
-def right(i, j):
-    ui = i-1
-    di = i+1
-    lj = j-1
-    rj = j+1
-    cnt = 1
-
-    while ui >= 0 and s[ui][j] != '#':
-        ui -= 1 
-        cnt += 1
-
-    while di < h and s[di][j] != '#':
-        di += 1 
-        cnt += 1
-
-    while lj >= 0 and s[i][lj] != '#':
-        lj -= 1 
-        cnt += 1
-
-    while rj < w and s[i][rj] != '#':
-        rj += 1 
-        cnt += 1
-    #print(i, j, ui, di, lj, rj, cnt)
-    return cnt
-
-make(L, R, U, D)
-
 ans = 0
-print(L)
-print(R)
-print(U)
-print(D)
+#print(L)
+#print(R)
+#print(U)
+#print(D)
 
 i = 0
 while i < h:
@@ -118,9 +88,5 @@ while i < h:
         j += 1
     i += 1
 
-'''
-for i in range(h):
-    for j in range(w):
-        ans = max(ans, L[i][j] + R[i][j] + U[i][j] + D[i][j] - 3)
-'''
 print(ans)
+'''
