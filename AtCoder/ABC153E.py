a2e7492b858dea730ca5a3ecc4c1ea373f824cf4
sourjp <hiro0209.ki@gmail.com>
2020-02-05 23:05:43 +0900

    atcoder

--- AtCoder/ABC153E.py
+++ AtCoder/ABC153E.py
@@ -1,4 +1,20 @@
 h, n = map(int, input().split())
+magic = [tuple(map(int, input().split())) for _ in range(n)]
+#magic = [list(map(int, input().split())) for _ in range(n)]
+
+dp = [float('inf')] * (h+1)
+dp[0] = 0
+
+for i in range(h):
+    for d, c in magic:
+        next_index = min(i+d, h)
+        dp[next_index] = min(dp[next_index], dp[i]+c)
+        #print(d, c, dp)
+
+print(dp[-1])
+
+'''
+h, n = map(int, input().split())
 
 attack = []
 magic = []
@@ -9,11 +25,12 @@ for i in range(n):
 
 dp = [[float('inf') for _ in range(h+1)] for _ in range(n+1)]
 dp[0][0] = 0
-print(attack, magic)
+#print(attack, magic)
 for i in range(n):
     for j in range(h+1):
         #print(max(dp[i][j], dp[i][j-attack[i]] + magic[i]))
         if dp[i+1][j] > dp[i][j]: dp[i+1][j] = dp[i][j]
-        if j-attack[i] >= 0 and dp[i+1][j] > dp[i][j-attack[i]] + magic[i]: dp[i+1][j] = dp[i][j-attack[i]] + magic[i]
+        if dp[i+1][min(j+attack[i], h)] > dp[i+1][j]+magic[i]: dp[i+1][min(j+attack[i], h)] = dp[i+1][j]+magic[i]
 
-print(dp)
+print(dp[-1][-1])
+'''
--- AtCoder/ABC153F.py
+++ AtCoder/ABC153F.py
@@ -1,30 +1,28 @@
+import heapq
+
 n, d, a = map(int, input().split())
 bom = []
 
-limit = 0
 for _ in range(n):
     x, h = map(int, input().split())
     bom.append([x, h])
-    limit = max(limit, x)
 
 bom.sort(key=lambda x:x[0])
 
-cnt = 0
-left = 0
-while left < n:
-    #print(bom)
-    if bom[left][1] > 0:
-        right = bom[left][0]+2*d+1
-        if bom[left][1] % a != 0: tmp = bom[left][1] // a + 1 
-        else: tmp = bom[left][1] // a
-        cnt += tmp
-        i = left
-        print(bom)
-        if right > limit: right = limit
-        while i < n and bom[i][0] <= right:
-            #print('r', right, bom[i], i)
-            bom[i][1] -= a * tmp
-            i += 1
-    left += 1
-print(cnt)
+ans = 0
+tot = 0
+q = []
+for x, h in bom:
+    while q:
+        if q[0][0] >= x: break
+        else:
+            _, t = heapq.heappop(q)
+            tot -= t
+    
+    r = max((h-tot-1)//a + 1, 0)
+    ans += r
+    tot += r * a
+    heapq.heappush(q, (x+2*d, r*a))
+
+print(ans)
 
